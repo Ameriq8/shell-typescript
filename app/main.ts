f9f66afc2fa12d5efc603186d5c05f0322d957ab
Ameriq8 <wwwwwwssssss91@gmail.com>
2025-11-05 17:58:17 +0300

    Refactor command handling in REPL to use switch-case structure

--- app/main.ts
+++ app/main.ts
@@ -9,11 +9,21 @@ rl.setPrompt("$ ");
 rl.prompt();
 
 rl.on("line", (input) => {
-  const command = input.trim();
-  if (command === "exit 0") {
-    process.exit(0);
-  } else {
-    console.log(`${command}: command not found`);
+  const command = input.trim().split(" ")[0];
+  const args = input.trim().split(" ").slice(1);
+
+  switch (command) {
+    case "exit 0":
+      process.exit(0);
+    case "echo":
+      if (args.length > 0) {
+        console.log(args.join(" "));
+      } else {
+        console.log();
+      }
+      break;
+    default:
+      console.log(`${command}: command not found`);
   }
   rl.prompt();
 });
