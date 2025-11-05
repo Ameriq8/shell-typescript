49f216513c919ae6cef15bb653bfb68608b68361
Ameriq8 <wwwwwwssssss91@gmail.com>
2025-11-05 18:08:20 +0300

    Refactor REPL command parsing to use destructuring for improved readability

--- app/main.ts
+++ app/main.ts
@@ -8,14 +8,22 @@ const rl = createInterface({
 rl.setPrompt("$ ");
 rl.prompt();
 
+type BuiltinCommand = "exit" | "echo" | "type";
+
+// Runtime array for lookup
+const builtins: BuiltinCommand[] = ["exit", "echo", "type"];
+
 rl.on("line", (input) => {
-  const command = input.trim().split(" ")[0];
-  const args = input.trim().split(" ").slice(1);
+  const [command, ...args] = input.trim().split(" ") as [
+    BuiltinCommand,
+    ...string[]
+  ];
 
   switch (command) {
     case "exit":
       const code = args.length > 0 ? parseInt(args[0], 10) : 0;
       process.exit(code);
+
     case "echo":
       if (args.length > 0) {
         console.log(args.join(" "));
@@ -23,6 +31,20 @@ rl.on("line", (input) => {
         console.log();
       }
       break;
+
+      case "type":
+        if (args.length === 0) {
+          console.log("type: missing operand");
+        }
+
+        if (builtins.includes(args[0] as BuiltinCommand)) {
+          console.log(`${args[0]} is a shell builtin`);
+        } else {
+          console.log(`${args[0]}: not found`);
+        }
+
+      break;
+
     default:
       console.log(`${command}: command not found`);
   }
